59b0a5c697cc8457c3760ab9327e58ed3295a897
Konstantin Levinsky <kvlevinsky@gmail.com>
2021-04-08 00:38:31 +0200

    Commit 2021-04-08--00-38

---
+++ Demo-MIDC/CounterClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Demo_MIDC {
+    internal class CounterClass {
+        protected int steps;
+
+        public CounterClass(int steps) {
+            this.steps = steps;
+        }
+
+        public void Run() {
+            for (int i = 0; i < steps; i++)
+                Console.WriteLine(Increment(i));
+        }
+
+        public int Increment(int value) {
+            Test("Method test invocation in the CounterClass");
+            return value + 1;
+        }
+
+        public string Test(string property) {
+            return property;
+        }
+    }
+}
---
+++ Demo-MIDC/Program.cs
@@ -0,0 +1,18 @@
+﻿using Demo_MIDC.Properties;
+using System;
+using System.Threading;
+
+namespace Demo_MIDC {
+    class Program {
+        static void Main(string[] args) {
+            while (true) {
+                CounterClass cc = new CounterClass(Settings.Default.Steps);
+                cc.Run();
+
+                Console.WriteLine("Increment cycle is over.\r\nSleeping for 5 seconds...");
+                Thread.Sleep(5000);
+                Console.Clear();
+            }
+        }
+    }
+}
