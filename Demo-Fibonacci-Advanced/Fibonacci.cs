499d73dbe55819e7ded0db348afd8f4970a38000
Konstantin Levinsky <kvlevinsky@gmail.com>
2021-04-09 23:10:17 +0200

    Commit 2021-04-05--23-10

---
+++ Demo-Fibonacci-Advanced/Fibonacci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Fibonacci_Advanced {
+    class Fibonacci {
+        protected Dictionary<ulong, ulong> cache = new Dictionary<ulong, ulong>();
+        public ulong Evaluate(ulong value) {
+            if (cache.TryGetValue(value, out ulong cachedValue)) return cachedValue;
+            if (value > 0) {
+                if (value == 1 || value == 2) return 1;
+                else {
+                    ulong result = Evaluate(value - 1) + Evaluate(value - 2);
+                    cache[value] = result;
+                    return result;
+                }
+            } else return 0;
+        }
+    }
+}
---
+++ Demo-Fibonacci-Advanced/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Fibonacci_Advanced {
+    class Program {
+        static void Main(string[] args) {
+            ulong Number = 1;
+            if (args.Length > 0)
+                ulong.TryParse(args[0], out Number);
+            Console.WriteLine($"Evaluating the {Number}-{GetEnding(Number)} Fibonacci number value...");
+            Console.WriteLine($"{Number}-{GetEnding(Number)} Fibonacci number value is {new Fibonacci().Evaluate(Number)}");
+            Console.Write("The sequence:");
+            for (ulong i = 1; i <= Number; i++)
+                Console.Write($" {new Fibonacci().Evaluate(i)}");
+            Console.ReadKey();
+        }
+
+        private static string GetEnding(ulong number) {
+            if (number == 1) return "st";
+            if (number == 2) return "nd";
+            if (number == 3) return "rd";
+            return "th";
+        }
+    }
+}
