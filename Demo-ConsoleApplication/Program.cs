74932423c49f09c7ff1fe9ca88b0f425ddc40ff0
Konstantin Levinsky <kvlevinsky@gmail.com>
2021-04-09 23:58:31 +0200

    Commit 2021-04-09--23-58

---
+++ Demo-ConsoleApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using MathOperationsStringed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_ConsoleApplication {
+    class Program {
+        static void Main(string[] args) {
+            string op1 = "123456789012345678901234567890";
+            string op2 = "9876543210987654321098765432109";
+            Console.WriteLine(MOS.ReverseString(MOS.Normalize(op1, 1 + Math.Max(op1.Length, op2.Length))));
+            Console.WriteLine("+");
+            Console.WriteLine(MOS.ReverseString(MOS.Normalize(op2, 1 + Math.Max(op1.Length, op2.Length))));
+            Console.WriteLine(new string('-', Math.Max(op1.Length, op2.Length)));
+            Console.WriteLine(MOS.Add(op1, op2));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathOperationsStringed/MOS.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperationsStringed {
+    public class MOS {
+        public static string Add(string operand1, string operand2) {
+            string result = "";
+            int maxLength = Math.Max(operand1.Length, operand2.Length);
+            operand1 = Normalize(operand1, maxLength);
+            operand2 = Normalize(operand2, maxLength);
+            byte digit1 = 0, digit2 = 0, digitCarry = 0;
+            for (int i = 0; i < maxLength; i++) {
+                if (!byte.TryParse(operand1[i].ToString(), out digit1)) break;
+                if (!byte.TryParse(operand2[i].ToString(), out digit2)) break;
+                int sum = digit1 + digit2 + digitCarry;
+                result += (sum % 10).ToString();
+                digitCarry = (byte)(sum / 10);
+            }
+            result += digitCarry.ToString();
+            result = ReverseString(result);
+            return result;
+        }
+
+        public static string Normalize(string operand, int maxLength) {
+            string result = ReverseString(operand) + new string('0', maxLength - operand.Length);
+            return result;
+        }
+
+        public static string ReverseString(string value) {
+            char[] temp = value.ToCharArray();
+            Array.Reverse(temp);
+            return new string(temp);
+        }
+    }
+}
